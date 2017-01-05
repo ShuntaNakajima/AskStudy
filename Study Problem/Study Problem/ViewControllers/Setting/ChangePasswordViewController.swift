d3349b597d1d8de5882727ed325d520228786d8e
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-01-06 07:10:48 +0900

    Update ChangePasswordViewController

--- Study Problem/Study Problem/ViewControllers/Setting/ChangePasswordViewController.swift
+++ Study Problem/Study Problem/ViewControllers/Setting/ChangePasswordViewController.swift
@@ -8,79 +8,80 @@
 
 import UIKit
 import Firebase
-import FirebaseAuth
-import FirebaseDatabase
-import FirebaseStorage
 import SVProgressHUD
 
 class ChangePasswordViewController: UIViewController {
-    @IBOutlet var oldPassword:UITextField!
-    @IBOutlet var newPassword:UITextField!
-    @IBOutlet var newPasswordAgain:UITextField!
-    var userDic = Dictionary<String, AnyObject>()
-    var Database = FIRDatabaseReference.init()
+    
+    @IBOutlet var oldPasswordField: UITextField!
+    @IBOutlet var newPasswordField: UITextField!
+    @IBOutlet var newPasswordAgainField: UITextField!
+    
+    var userDic: [String: AnyObject] = [:]
+    let databaseReference: FIRDatabaseReference = FIRDatabase.database().reference()
     override func viewDidLoad() {
         super.viewDidLoad()
-        Database = FIRDatabase.database().reference()
-        Database.child("user").child((FIRAuth.auth()?.currentUser!.uid)!).observe(.value, with: { snapshot in
-            if var userDictionary = snapshot.value as? Dictionary<String, AnyObject> {
+        
+        databaseReference.child("user").child((FIRAuth.auth()?.currentUser!.uid)!).observe(.value, with: { snapshot in
+            
+            if var userDictionary: [String: AnyObject] = snapshot.value as? Dictionary<String, AnyObject> {
+                
                 let key = snapshot.key
                 userDictionary["key"] = key as AnyObject?
-               self.userDic = userDictionary
+                self.userDic = userDictionary
             }
         })
     }
+    
     @IBAction func done(){
+        
         SVProgressHUD.show()
-        let credential = FIREmailPasswordAuthProvider.credential(withEmail: userDic["email"] as! String, password: oldPassword.text!)
+        guard let oldPassword: String = oldPasswordField.text else {
+            
+            return
+        }
+        let credential = FIREmailPasswordAuthProvider.credential(withEmail: userDic["email"] as! String, password: oldPassword)
         FIRAuth.auth()?.currentUser?.reauthenticate(with: credential, completion: { (error) in
+            
             if error != nil{
+                
                 let alertController = UIAlertController(title: "Please check your old password", message: "", preferredStyle: .alert)
                 let otherAction = UIAlertAction(title: "OK", style: .default)
                 alertController.addAction(otherAction)
                 self.present(alertController, animated: true, completion: nil)
                 SVProgressHUD.dismiss()
             }else{
-                if self.newPassword.text! == self.newPasswordAgain.text! && self.newPassword.text! != ""{
-                    let user = FIRAuth.auth()?.currentUser
-                    user?.updatePassword(self.newPassword.text!) { error in
-                        if let error = error {
-                            print(error)
-                            let alertController = UIAlertController(title: "The password must be 6 characters long or more.", message: "", preferredStyle: .alert)
-                            let otherAction = UIAlertAction(title: "OK", style: .default)
-                            alertController.addAction(otherAction)
-                            self.present(alertController, animated: true, completion: nil)
-                            SVProgressHUD.dismiss()
-                        } else {
-                            SVProgressHUD.showSuccess(withStatus: "Update Successful!")
-                            let delayTime = DispatchTime.now() + Double(Int64(1 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
-                            DispatchQueue.main.asyncAfter(deadline:delayTime, execute:{ SVProgressHUD.dismiss() })
-                        }
-                    }
-                }else{
+                
+                guard let newPassword: String = newPasswordField.text, let newPasswordAgain: String = newPasswordAgainField.text else {
+                    
                     let alertController = UIAlertController(title: "New password dosen't match. please check it", message: "", preferredStyle: .alert)
                     let otherAction = UIAlertAction(title: "OK", style: .default)
                     alertController.addAction(otherAction)
                     self.present(alertController, animated: true, completion: nil)
                     SVProgressHUD.dismiss()
+                    return
+                }
+                
+                
+                let user = FIRAuth.auth()?.currentUser
+                user?.updatePassword(newPassword) { error in
+                    
+                    if let error = error {
+                        
+                        print(error)
+                        let alertController = UIAlertController(title: "The password must be 6 characters long or more.", message: "", preferredStyle: .alert)
+                        let otherAction = UIAlertAction(title: "OK", style: .default)
+                        alertController.addAction(otherAction)
+                        self.present(alertController, animated: true, completion: nil)
+                        SVProgressHUD.dismiss()
+                        
+                    } else {
+                        
+                        SVProgressHUD.showSuccess(withStatus: "Update Successful!")
+                        let delayTime = DispatchTime.now() + Double(Int64(1 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+                        DispatchQueue.main.asyncAfter(deadline:delayTime, execute:{ SVProgressHUD.dismiss() })
+                    }
                 }
             }
         })
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
