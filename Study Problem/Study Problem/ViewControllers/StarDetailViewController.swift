78e2aa48a42ebb4183b367c203219ead60a27e68
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-01-06 07:12:32 +0900

    Update StarDetailViewController

--- Study Problem/Study Problem/ViewControllers/StarDetailViewController.swift
+++ Study Problem/Study Problem/ViewControllers/StarDetailViewController.swift
@@ -8,23 +8,24 @@
 
 import UIKit
 
-class StarDtialViewController: UIViewController {
+class StarDetailViewController: UIViewController {
+    
     @IBOutlet var closebutton:UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         closebutton.layer.cornerRadius=30
         closebutton.layer.masksToBounds=true
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         self.view.backgroundColor = UITabBar.appearance().tintColor
         self.closebutton.setTitleColor(UITabBar.appearance().tintColor, for: .normal)
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
     @IBAction func close(){
         self.dismiss(animated: true, completion: nil)
     }
