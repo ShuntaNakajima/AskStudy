5d671f45b3c3a38b88416823f159f2bafbf2d611
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-01-06 07:10:34 +0900

    Update ChangeEmailViewController

--- Study Problem/Study Problem/ViewControllers/Setting/ChangeEmailViewController.swift
+++ Study Problem/Study Problem/ViewControllers/Setting/ChangeEmailViewController.swift
@@ -8,47 +8,53 @@
 
 import UIKit
 import Firebase
-import FirebaseAuth
-import FirebaseDatabase
-import FirebaseStorage
 import SVProgressHUD
 
-class ChangeEmailViewController: UIViewController,UITextFieldDelegate {
+class ChangeEmailViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet var emailTextField:UITextField!
+    @IBOutlet var emailTextField: UITextField!
     
-    var userDic=Dictionary<String, AnyObject>()
-    var Database = FIRDatabaseReference.init()
+    var userDic: [String: AnyObject] = [:]
+    let databaseReference: FIRDatabaseReference = FIRDatabase.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Database = FIRDatabase.database().reference()
+        
         emailTextField.delegate = self
-        Database.child("user").child((FIRAuth.auth()?.currentUser!.uid)!).observe(.value, with: { snapshot in
-            if var postDictionary = snapshot.value as? Dictionary<String, AnyObject> {
+        databaseReference.child("user").child((FIRAuth.auth()?.currentUser!.uid)!).observe(.value, with: { snapshot in
+            
+            if var postDictionary: [String: AnyObject] = snapshot.value as? Dictionary<String, AnyObject> {
                 let key = snapshot.key
                 postDictionary["key"] = key as AnyObject?
                 self.userDic = postDictionary
-                self.emailTextField.text = self.userDic["email"] as! String?
+                self.emailTextField.text = self.userDic["email"] as? String
             }
         })
     }
     
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
-        // キーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
         textField.resignFirstResponder()
         return true
     }
     
     @IBAction func changeEmail(){
+        
         SVProgressHUD.show()
         let user = FIRAuth.auth()?.currentUser
         user?.updateEmail(emailTextField.text!) { error in
+            
             if let error = error {
                 print(error)
                 SVProgressHUD.dismiss()
+                
             } else {
-                self.Database.child("user").child((FIRAuth.auth()?.currentUser!.uid)!).child("email").setValue(self.emailTextField.text!)
+                
+                guard let text: String = emailTextField.text else {
+                    
+                    return
+                }
+                databaseReference.child("user").child((FIRAuth.auth()?.currentUser!.uid)!).child("email").setValue(text)
                 SVProgressHUD.showSuccess(withStatus: "Update Successful!")
                 let delayTime = DispatchTime.now() + Double(Int64(1 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
                 DispatchQueue.main.asyncAfter(deadline:delayTime, execute:{ SVProgressHUD.dismiss() })
