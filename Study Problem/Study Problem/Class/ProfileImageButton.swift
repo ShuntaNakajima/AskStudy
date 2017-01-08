b7db3c9affe8634f2d5779e01571ffebe6c4b1a7
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-01-08 13:26:22 +0900

    Update ProfileImageButton

--- Study Problem/Study Problem/Class/ProfileImageButton.swift
+++ Study Problem/Study Problem/Class/ProfileImageButton.swift
@@ -8,23 +8,28 @@
 
 import Foundation
 import UIKit
-class ProfileImageButtonClass: UIButton{
+
+class ProfileImageButton: UIButton{
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.updateLayout()
     }
+    
     override func awakeFromNib() {
     }
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.updateLayout()
     }
+    
     func updateLayout(){
         self.layer.borderColor = UIColor.white.cgColor
         self.imageView!.layer.borderWidth = 10
         self.imageView!.layer.cornerRadius = 25
-        self.layer.cornerRadius=25
-        self.layer.masksToBounds=true
+        self.layer.cornerRadius = 25
+        self.layer.masksToBounds = true
         self.setTitle("", for: UIControlState.normal)
         self.setBackgroundImage(UIImage(named: "noimage.gif")!, for: .normal)
     }
