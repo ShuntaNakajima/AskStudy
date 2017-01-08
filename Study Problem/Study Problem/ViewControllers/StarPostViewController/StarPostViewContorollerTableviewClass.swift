721b2cb710b7b7396da37994aeee8d82103c9aad
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-01-08 13:36:57 +0900

    Update StarPostViewController extension

--- Study Problem/Study Problem/ViewControllers/StarPostViewController/StarPostViewContorollerTableviewClass.swift
+++ Study Problem/Study Problem/ViewControllers/StarPostViewController/StarPostViewContorollerTableviewClass.swift
@@ -9,63 +9,65 @@
 
 import Foundation
 import UIKit
-import FirebaseStorage
-import FirebaseAuth
-import FirebaseDatabase
+import Firebase
 
-extension StarPostViewController:UITableViewDataSource,UITableViewDelegate{
+extension StarPostViewController: UITableViewDataSource, UITableViewDelegate{
 
 
-    private func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return posts.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostTableViewCell
-        let post = posts[indexPath.row]
-        let postDictionary = post as? Dictionary<String, AnyObject>
+        let cell: PostTableViewCell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostTableViewCell
+        let post: [String: AnyObject] = posts[indexPath.row]
         for i in cell.view.subviews{
             i.removeFromSuperview()
         }
-        cell.replyscountLabel.text = String(postDictionary!["reply"] as! Int!)
-        cell.subjectLabel.text = postDictionary!["subject"] as? String!
-        let postdate = postDictionary!["date"] as! String!
-        let now = Date()
-        cell.dateLabel.text = now.offset(toDate: (postdate?.postDate())!)
-        cell.textView.text = postDictionary!["text"] as? String
-        let currentUser = Database.child("user").child((postDictionary!["author"] as? String)!)
+        cell.replyscountLabel.text = String(post["reply"] as! Int!)
+        cell.subjectLabel.text = post["subject"] as? String!
+        let postdate = post["date"] as! String!
+        cell.dateLabel.text = Date().offset(toDate: (postdate?.postDate())!)
+        cell.textView.text = post["text"] as? String
+        let currentUser = ref.child("user").child((post["author"] as? String)!)
         currentUser.observe(FIRDataEventType.value, with: { snapshot in
+            
             let postUser = (snapshot.value! as AnyObject)["username"] as! String
             cell.profileLabel.text = postUser
-        }, withCancel: { (error) in
+        }, withCancel: { error in
+            
             print(error)
         })
-        DispatchQueue.global().async(execute:{
+        DispatchQueue.global().async(execute: {
+            
             var viewImg = UIImage()
-            let storage = FIRStorage.storage()
-            let storageRef = storage.reference(forURL: "gs://studyproblemfirebase.appspot.com/user")
-            let autorsprofileRef = storageRef.child("\((postDictionary!["author"] as? String)!)/profileimage.png")
-            autorsprofileRef.data(withMaxSize: 1 * 1028 * 1028) { (data, error) -> Void in
+            let storageRef = FIRStorage.storage().reference(forURL: "gs://studyproblemfirebase.appspot.com/user")
+            let autorsprofileRef = storageRef.child("\(post["author"] as! String)/profileimage.png")
+            autorsprofileRef.data(withMaxSize: 1 * 1028 * 1028) { data, error in
                 if error != nil {
-                    print(error)
+                    
+                    print("\(error)")
                 } else {
+                    
                     viewImg = data.flatMap(UIImage.init)!
                     DispatchQueue.main.async(execute: {
+                        
                         cell.profileImage.setBackgroundImage(viewImg, for: UIControlState.normal)
                         cell.layoutSubviews()
-                    });
+                    })
                 }
             }
-        });
+        })
         cell.view.translatesAutoresizingMaskIntoConstraints = false
-        cell.setNib(photos: postDictionary!["Photo"] as! Int,key:postDictionary!["key"] as! String,on:self)
+        cell.setNib(photos: post["Photo"] as! Int, key:post["key"] as! String,on:self)
         cell.profileImage.tag = indexPath.row
         cell.profileImage.addTarget(self, action: #selector(MainViewController.showUserData(sender:)), for: .touchUpInside)
         return cell
     }
+    
     func tableView(_ table: UITableView, didSelectRowAt indexPath:IndexPath) {
+        
         let post = posts[indexPath.row]
         let postDictionary = post
         selectpost = postDictionary["key"] as! String!
@@ -74,34 +76,39 @@ extension StarPostViewController:UITableViewDataSource,UITableViewDelegate{
         }
     }
     func cellLongPressed(recognizer: UILongPressGestureRecognizer) {
+        
         let point = recognizer.location(in: tableView)
         let indexPath = tableView.indexPathForRow(at: point)
         if indexPath == nil {
+            
         } else if recognizer.state == UIGestureRecognizerState.began  {
-            if longState == false{
+            
+            if !longState {
                 longState = true
-                let Database = FIRDatabase.database().reference()
-                let recentUesrsQuery = Database.child("user").child((FIRAuth.auth()?.currentUser!.uid)!).child("stars").queryOrdered(byChild: "userstars").queryEqual(toValue: self.posts[indexPath!.row]["key"] as! String!)
+                let reference: FIRDatabaseReference = FIRDatabase.database().reference()
+                let recentUesrsQuery = reference.child("user").child((FIRAuth.auth()?.currentUser!.uid)!).child("stars").queryOrdered(byChild: "userstars").queryEqual(toValue: self.posts[indexPath!.row]["key"] as! String!)
                 recentUesrsQuery.observe(.value, with: { snapshot in
+                    
                     var mykey = ""
-                    print(snapshot.value)
                     if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
-                        print(snapshots)
+                        
                         for snap in snapshots {
                             mykey = snap.key
                         }
                     }
                     if mykey == ""{
-                        if self.longState == true{
-                            let newFollowChild = Database.child("user/\((FIRAuth.auth()?.currentUser!.uid)!)/stars/").childByAutoId().child("userstars")
+                        if self.longState {
+                            
+                            let newFollowChild = self.ref.child("user/\((FIRAuth.auth()?.currentUser!.uid)!)/stars/").childByAutoId().child("userstars")
                             newFollowChild.setValue(self.posts[indexPath!.row]["key"] as! String!)
                             let anImage = UIImage(named: "star.gif")
-                            ToastView.showText(text: "Star", image: anImage!, imagePosition: .Left, duration:.Short)
+                            _ = ToastView.showText(text: "Star", image: anImage!, imagePosition: .Left, duration:.Short)
                             self.longState = false
                         }
                     }else{
-                        if self.longState == true{
-                            Database.child("user/\((FIRAuth.auth()?.currentUser!.uid)!)/stars/").child(mykey).child("userstars").removeValue()
+                        if self.longState {
+                            
+                            self.ref.child("user/\((FIRAuth.auth()?.currentUser!.uid)!)/stars/").child(mykey).child("userstars").removeValue()
                             if self.posts.count == 1{
                                 self.posts = []
                                 self.tableView.reloadData()
@@ -109,7 +116,7 @@ extension StarPostViewController:UITableViewDataSource,UITableViewDelegate{
                                 self.reload()
                             }
                             let anImage = UIImage(named: "star.gif")
-                            ToastView.showText(text: "UnStar", image: anImage!, imagePosition: .Left, duration:.Short)
+                            _ = ToastView.showText(text: "UnStar", image: anImage!, imagePosition: .Left, duration:.Short)
                             self.longState = false
                         }
                     }
