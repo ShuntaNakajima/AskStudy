fd37f0e8ef161b291dcecf6b1ca32c70d5df8fc6
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-01-08 14:01:58 +0900

    Update ThemeColorViewController

--- Study Problem/Study Problem/ThemeColor/ThemeColorViewController.swift
+++ Study Problem/Study Problem/ThemeColor/ThemeColorViewController.swift
@@ -10,64 +10,81 @@ import UIKit
 
 
 class ThemeColorViewController: UIViewController {
-let color = UserDefaults.standard
-    @IBOutlet var YellowButton : ThemeButton!
-    @IBOutlet var LightBlueButton : ThemeButton!
-    @IBOutlet var BlueButton : ThemeButton!
-    @IBOutlet var RedButton : ThemeButton!
-    @IBOutlet var GreenButton : ThemeButton!
-    @IBOutlet var PurpleButton : ThemeButton!
-    @IBOutlet var OrangeButton : ThemeButton!
+    
+    let userDefaults: UserDefaults = UserDefaults.standard
+    
+    @IBOutlet var yellowButton : ThemeButton!
+    @IBOutlet var lightBlueButton : ThemeButton!
+    @IBOutlet var blueButton : ThemeButton!
+    @IBOutlet var redButton : ThemeButton!
+    @IBOutlet var greenButton : ThemeButton!
+    @IBOutlet var purpleButton : ThemeButton!
+    @IBOutlet var orangeButton : ThemeButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        YellowButton.backgroundColor = UIColor.themeYellow()
-        LightBlueButton.backgroundColor = UIColor.themeLightBlue()
-        BlueButton.backgroundColor = UIColor.themeBlue()
-        PurpleButton.backgroundColor = UIColor.themePurple()
-        RedButton.backgroundColor = UIColor.themeRed()
-        GreenButton.backgroundColor = UIColor.themeGreen()
-        OrangeButton.backgroundColor = UIColor.themeOrange()
+        yellowButton.backgroundColor = UIColor.themeYellow()
+        lightBlueButton.backgroundColor = UIColor.themeLightBlue()
+        blueButton.backgroundColor = UIColor.themeBlue()
+        purpleButton.backgroundColor = UIColor.themePurple()
+        redButton.backgroundColor = UIColor.themeRed()
+        greenButton.backgroundColor = UIColor.themeGreen()
+        orangeButton.backgroundColor = UIColor.themeOrange()
     }
+    
     @IBAction func colorYellow(){
+        
         self.navigationController?.navigationBar.barTintColor = UIColor.themeYellow()
-       UITabBar.appearance().tintColor = UIColor.themeYellow()
-    UINavigationBar.appearance().barTintColor = UIColor.themeYellow()
-        color.set("yellow", forKey: "id")
+        UITabBar.appearance().tintColor = UIColor.themeYellow()
+        UINavigationBar.appearance().barTintColor = UIColor.themeYellow()
+        userDefaults.set("yellow", forKey: "id")
     }
+    
     @IBAction func colorLightBlue(){
-         self.navigationController?.navigationBar.barTintColor = UIColor.themeLightBlue()
-     UINavigationBar.appearance().barTintColor = UIColor.themeLightBlue()
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.themeLightBlue()
+        UINavigationBar.appearance().barTintColor = UIColor.themeLightBlue()
         UITabBar.appearance().tintColor = UIColor.themeLightBlue()
-        color.set("lightblue", forKey: "id")
+        userDefaults.set("lightblue", forKey: "id")
     }
+    
     @IBAction func colorBlue(){
-         self.navigationController?.navigationBar.barTintColor = UIColor.themeBlue()
-      UINavigationBar.appearance().barTintColor = UIColor.themeBlue()
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.themeBlue()
+        UINavigationBar.appearance().barTintColor = UIColor.themeBlue()
         UITabBar.appearance().tintColor = UIColor.themeBlue()
-        color.set("blue", forKey: "id")
+        userDefaults.set("blue", forKey: "id")
     }
+    
     @IBAction func colorRed(){
-         self.navigationController?.navigationBar.barTintColor = UIColor.themeRed()
-         UINavigationBar.appearance().barTintColor = UIColor.themeRed()
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.themeRed()
+        UINavigationBar.appearance().barTintColor = UIColor.themeRed()
         UITabBar.appearance().tintColor = UIColor.themeRed()
-        color.set("red", forKey: "id")
+        userDefaults.set("red", forKey: "id")
     }
+    
     @IBAction func colorGreen(){
-         self.navigationController?.navigationBar.barTintColor = UIColor.themeGreen()
-         UITabBar.appearance().tintColor = UIColor.themeGreen()
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.themeGreen()
+        UITabBar.appearance().tintColor = UIColor.themeGreen()
         UINavigationBar.appearance().barTintColor = UIColor.themeGreen()
-        color.set("green", forKey: "id")
+        userDefaults.set("green", forKey: "id")
     }
+    
     @IBAction func colorOrange(){
-         self.navigationController?.navigationBar.barTintColor = UIColor.themeOrange()
-       UITabBar.appearance().tintColor = UIColor.themeOrange()
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.themeOrange()
+        UITabBar.appearance().tintColor = UIColor.themeOrange()
         UINavigationBar.appearance().barTintColor = UIColor.themeOrange()
-        color.set("orange", forKey: "id")
+        userDefaults.set("orange", forKey: "id")
     }
+    
     @IBAction func colorPurple(){
-         self.navigationController?.navigationBar.barTintColor = UIColor.themePurple()
-         UITabBar.appearance().tintColor = UIColor.themePurple()
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.themePurple()
+        UITabBar.appearance().tintColor = UIColor.themePurple()
         UINavigationBar.appearance().barTintColor = UIColor.themePurple()
-        color.set("purple", forKey: "id")
+        userDefaults.set("purple", forKey: "id")
     }
 }
